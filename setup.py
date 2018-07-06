e0997f0ae3d6aa13a0e4ac568b6b8604fe49c27c
Unknown <andreas.verleysen@gmail.com>
2018-07-06 12:25:29 +0200

    change setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='orb_catching_game',
     version='1.0',
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    description='A game where a robot has to catch an orb in increasing difficult levels. Mean for RL purposes.',
     author='Andreas Verleysen'
 )
