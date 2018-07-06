665f134d8965451eb1128446a201215a0c709edb
Unknown <andreas.verleysen@gmail.com>
2018-07-06 13:16:18 +0200

    setup.py include settings.json file as data package

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name='orb_catching_game',
     version='1.0',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    data_files=[('SETTINGS.json')],
+    include_package_data=True,
     description='A game where a robot has to catch an orb in increasing difficult levels. Mean for RL purposes.',
     author='Andreas Verleysen'
 )
